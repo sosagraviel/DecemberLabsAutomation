eb92a0f25f7cb62a8cbc1597561c6af26da70b90
graviel <sosagraviel@gmail.com>
2020-11-23 00:33:54 -0300

    Update chromeDriver

--- src/test/java/stepsDefinitions/stepsDefinitionLogin.java
+++ src/test/java/stepsDefinitions/stepsDefinitionLogin.java
@@ -6,19 +6,32 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.nio.file.Paths;
+
 public class stepsDefinitionLogin extends baseClass {
+    public static final String CHROMEDRIVERMAC = "chromedrivermac";
+    public static final String CHROMEDRIVERLINUX = "chromedriverlinux";
+    public static final String CHROMEDRIVER_EXE = "chromedriver.exe";
+    private static String OS = System.getProperty("os.name").toLowerCase();
     public LoginPage loginPage;
     final baseClass base;
 
-    public stepsDefinitionLogin(baseClass base){
-        this.base=base;
+    public stepsDefinitionLogin(baseClass base) {
+        this.base = base;
     }
 
     @Given("I Launch Chrome Browser")
     public void i_Launch_Chrome_Browser() {
-        System.setProperty("webdriver.chrome.driver","//home/graviel/Descargas/intelij/chromedriver");
-        base.basewebDriver=new ChromeDriver();
-        loginPage =new LoginPage(base.basewebDriver);
+        String browser = CHROMEDRIVERLINUX;
+        if (isWindows()) {
+            browser = CHROMEDRIVER_EXE;
+        } else if (isMac()) {
+            browser = CHROMEDRIVERMAC;
+        }
+        System.setProperty("webdriver.chrome.driver", String.valueOf(Paths.get("src/test/drivers/", browser)));
+
+        base.basewebDriver = new ChromeDriver();
+        loginPage = new LoginPage(base.basewebDriver);
     }
 
     @When("A user is in the homepage {string}")
@@ -35,10 +48,17 @@ public class stepsDefinitionLogin extends baseClass {
 
 
     @When("Click on Login")
-    public void click_on_Login(){
+    public void click_on_Login() {
         loginPage.clickOnLogin();
 
     }
 
+    private boolean isWindows() {
+        return (OS.indexOf("win") >= 0);
+    }
+
+    private boolean isMac() {
+        return (OS.indexOf("mac") >= 0);
+    }
 
 }
